f02f48cbf0f526f8be149b589e8db065d5170dd1
Ramana-pedavegi <pedavegi72@gmail.com>
2026-06-25 12:03:42 +0530

    added SumOf2dArray program

---
+++ SumOf2dArray.java
@@ -0,0 +1,15 @@
+public class SumOf2dArray {                      
+    static int sum=0;
+
+    public static void main(String[] args) {
+        int[][] arr = {{1,8,4},{9,7,2},{7,6,4}};
+        for(int i=0;i<arr.length;i++){
+            for(int j =0;j<arr[i].length;j++){
+                    sum+=arr[i][j];
+}
+ }
+                    System.out.println("sum :"+sum);
+
+    }
+    
+}
