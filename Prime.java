a226b32f158a45d2e27abd9b8e3b3b0b51325872
Ramana-pedavegi <pedavegi72@gmail.com>
2026-06-26 22:58:52 +0530

    added Prime program

---
+++ Prime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your value");
+        int n = sc.nextInt();
+        int count =0;
+        if(n>1){
+            for(int i=1; i<=n; i++){
+                if(n%i==0){
+                    count++;
+                }
+            }
+            if(count==2){
+                System.out.println("Number is  prime");
+            }else {
+                    System.out.println("Number is not prime");
+
+            }
+        }else{
+            System.out.println("Number is not prime");
+        }
+        sc.close();
+    }
+    
+}
