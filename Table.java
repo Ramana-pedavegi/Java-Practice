a02607dfaacdaabed5915111f876e6d9fbbcef9f
Ramana-pedavegi <pedavegi72@gmail.com>
2026-06-30 20:05:23 +0530

    added Table program

---
+++ Table.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number");
+        int n = sc.nextInt();
+        for(int i=0;i<=n;i++){
+            System.out.println(n*i);
+        }
+        sc.close();
+    }
+    
+}
